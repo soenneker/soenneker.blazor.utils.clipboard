5b22e8d501b2bc4519df7b4f420f36c2b93f4a11
soenneker <jake@soenneker.com>
2026-09-25 07:57:00 -0500

    Updated Multiple NuGet packages

--- src/Soenneker.Blazor.Utils.Clipboard/Enums/ClipboardPermissionState.cs
+++ src/Soenneker.Blazor.Utils.Clipboard/Enums/ClipboardPermissionState.cs
@@ -1,19 +1,22 @@
+using Soenneker.Gen.EnumValues;
+
 namespace Soenneker.Blazor.Utils.Clipboard.Enums;
 
 /// <summary>
 /// Result of querying clipboard read or write permission.
 /// </summary>
-public enum ClipboardPermissionState
+[EnumValue]
+public sealed partial class ClipboardPermissionState
 {
     /// <summary>Permission granted.</summary>
-    Granted,
+    public static readonly ClipboardPermissionState Granted = new(0);
 
     /// <summary>Permission denied.</summary>
-    Denied,
+    public static readonly ClipboardPermissionState Denied = new(1);
 
     /// <summary>Permission not yet requested; user may be prompted.</summary>
-    Prompt,
+    public static readonly ClipboardPermissionState Prompt = new(2);
 
     /// <summary>Permission query not supported (e.g. in Firefox/Safari).</summary>
-    Unsupported
+    public static readonly ClipboardPermissionState Unsupported = new(3);
 }
